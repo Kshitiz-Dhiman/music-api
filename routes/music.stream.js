6c25f4ea6ca8cdd48d3e932df8b5613f66067b82
Kshitiz <129819534+Kshitiz-Dhiman@users.noreply.github.com>
2025-01-26 04:05:31 +0530

    using direct url instead of ffmpeg

--- routes/music.stream.js
+++ routes/music.stream.js
@@ -6,6 +6,7 @@ import { param, validationResult } from 'express-validator';
 const router = express.Router();
 const ffmpegPath = 'C:/ffmpeg/bin/ffmpeg.exe';
 
+
 const validateYouTubeId = [
     param('youtubeId').isString().withMessage('Invalid YouTube ID'),
 ];
@@ -13,33 +14,31 @@ const validateYouTubeId = [
 router.get("/:youtubeId", validateYouTubeId, async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        console.error("Validation errors:", errors.array());
         return res.status(400).json({ errors: errors.array() });
     }
 
     try {
-        console.log("Fetching video info for ID:", req.params.youtubeId);
         const videoInfo = await ytdl.getInfo(req.params.youtubeId);
         const format = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio', filter: 'audioonly' });
 
-        console.log("Starting ffmpeg stream with format URL:", format.url);
-        const stream = ffmpeg(format.url)
-            .setFfmpegPath(ffmpegPath)
-            .audioCodec('libmp3lame')
-            .format('mp3')
-            .on('end', () => {
-                console.log('All done! Processing finished successfully');
-            })
-            .on('error', (err) => {
-                console.error('Whoops! Error in processing:', err);
-                if (!res.headersSent) {
-                    res.status(500).json({ error: 'Something went wrong with the stream' });
-                }
-            });
+        // const stream = ffmpeg(format.url)
+        //     .setFfmpegPath(ffmpegPath)
+        //     .audioCodec('libmp3lame')
+        //     .format('mp3')
+        //     .on('end', () => {
+        //         console.log('All done! Processing finished successfully');
+        //     })
+        //     .on('error', (err) => {
+        //         console.error('Whoops! Error in processing:', err);
+        //         if (!res.headersSent) {
+        //             res.status(500).json({ error: 'Something went wrong with the stream' });
+        //         }
+        //     });
 
         res.setHeader('Content-Type', 'audio/mpeg');
-        res.setHeader('Transfer-Encoding', 'chunked');
-        stream.pipe(res, { end: true });
+        // res.setHeader('Transfer-Encoding', 'chunked');
+        // stream.pipe(res, { end: true });
+        res.json({ url: format.url });
     } catch (e) {
         console.error("Error fetching video info:", e);
         res.status(500).json({ error: "Internal Server Error" });
