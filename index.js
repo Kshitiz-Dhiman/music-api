f0ea6a41e352a4cc7970f4a2f14dd5e12b114edf
Kshitiz <129819534+Kshitiz-Dhiman@users.noreply.github.com>
2025-01-26 03:30:28 +0530

    initialize Express server with music streaming and home routes

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+//Routes
+import musicStream from "./routes/music.stream.js";
+import homePage from "./routes/music.home.js";
+
+app.use("/stream", musicStream);
+app.use("/home", homePage);
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
---
+++ routes/music.stream.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import ytdl from 'ytdl-core';
+import ffmpeg from 'fluent-ffmpeg';
+import { param, validationResult } from 'express-validator';
+
+const router = express.Router();
+const ffmpegPath = 'C:/ffmpeg/bin/ffmpeg.exe';
+
+
+const validateYouTubeId = [
+    param('youtubeId').isString().withMessage('Invalid YouTube ID'),
+];
+
+router.get("/:youtubeId", validateYouTubeId, async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const videoInfo = await ytdl.getInfo(req.params.youtubeId);
+        const format = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio', filter: 'audioonly' });
+
+        const stream = ffmpeg(format.url)
+            .setFfmpegPath(ffmpegPath)
+            .audioCodec('libmp3lame')
+            .format('mp3')
+            .on('end', () => {
+                console.log('All done! Processing finished successfully');
+            })
+            .on('error', (err) => {
+                console.error('Whoops! Error in processing:', err);
+                if (!res.headersSent) {
+                    res.status(500).json({ error: 'Something went wrong with the stream' });
+                }
+            });
+
+        res.setHeader('Content-Type', 'audio/mpeg');
+        res.setHeader('Transfer-Encoding', 'chunked');
+        stream.pipe(res, { end: true });
+    } catch (e) {
+        console.error("Error fetching video info:", e);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+export default router;
