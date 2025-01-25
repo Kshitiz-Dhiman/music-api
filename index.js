7c17a0b9cdc428b20fa86857014fedd8230db5f6
Kshitiz <129819534+Kshitiz-Dhiman@users.noreply.github.com>
2025-01-26 03:32:11 +0530

    Just added a "/" route

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ import homePage from "./routes/music.home.js";
 
 app.use("/stream", musicStream);
 app.use("/home", homePage);
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
