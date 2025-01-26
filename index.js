bd9368b3556533541924edcdebb12e6433603dc4
Kshitiz <129819534+Kshitiz-Dhiman@users.noreply.github.com>
2025-01-26 05:55:07 +0530

    refactor: update root response message to "Hello world"

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const homePage = require("./routes/music.home.js");
 app.use("/stream", musicStream);
 app.use("/home", homePage);
 app.get("/", (req, res) => {
-    res.json("Music rahh");
+    res.json("Hello world");
 });
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
