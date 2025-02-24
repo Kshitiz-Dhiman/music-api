bc5360015b0a610d6e4e320999701e5dbece927e
Kshitiz <129819534+Kshitiz-Dhiman@users.noreply.github.com>
2025-02-25 02:21:14 +0530

    fix: change server port from 3000 to 4000

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ app.get("/", (req, res) => {
         message: "Welcome to the music app"
     });
 });
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(4000, () => {
+    console.log("Server is running on port 4000");
 });
